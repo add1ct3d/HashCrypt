d06d736fe49acb0db6004f6be85e5ece09d8ef05
Isaacdelly <isaacdelly@gmail.com>
2018-01-17 21:02:31 -0800

    Migrated CRC-32 file to main hashcrypt.c

--- hashcrypt.c
+++ hashcrypt.c
@@ -4,12 +4,15 @@
 #include <stdbool.h>
 #include <math.h>
 #include <conio.h>
+#include <inttypes.h>
 
 void encrypt(char*, char*);
 void decrypt(char* filename, char* seed);
 long getFileSize(char* filename);
 bool isFile(char* filename);
 char* stripDash(char* string);
+uint32_t rc_crc32(uint32_t crc, const char *buf, size_t len);
+int hash(const char *s);
 
 int main(int argc, char* argv[]) {
 
@@ -40,7 +43,7 @@ int main(int argc, char* argv[]) {
                 if (argv[i][0] == '-') {
                     strcat(seed, stripDash(argv[i])); }
                 else {
-                  strcat(seed, argv[i]); }
+                    strcat(seed, argv[i]); }
             }
         }
 
@@ -57,7 +60,9 @@ int main(int argc, char* argv[]) {
 }
 
 void encrypt(char* filename, char* seed) {
-    printf("ENCRYPT:\nFile Name: %s\nSeed: %s\nHash: %d\n", filename, seed, hash(seed)); }
+    printf("ENCRYPT:\nFile Name: %s\nSeed: %s\nHash: %d\n", filename, seed, hash(seed));
+
+    }
 
 void decrypt(char* filename, char* seed) {
     printf("DECRYPT:\nFile Name: %s\nSeed: %s\nHash: %d\n", filename, seed, hash(seed)); }
@@ -88,5 +93,45 @@ bool isFile(char* filename) {
 // Strip first char '-' from string
 char* stripDash(char* string) {
     return strchr(string, string[1]);
+}
+
+// CRC-32 Hashing Algorithm
+uint32_t rc_crc32(uint32_t crc, const char *buf, size_t len) {
+	static uint32_t table[256];
+	static int have_table = 0;
+	uint32_t rem;
+	uint8_t octet;
+	int i, j;
+	const char *p, *q;
+
+	/* Not thread safe - no mutex */
+	if (have_table == 0) {
+		/* Generate CRC table */
+		for (i = 0; i < 256; i++) {
+			rem = i;
+			for (j = 0; j < 8; j++) {
+				if (rem & 1) {
+					rem >>= 1;
+					rem ^= 0xedb88320;
+				} else
+					rem >>= 1;
+			}
+			table[i] = rem;
+		}
+		have_table = 1;
+	}
+
+	crc = ~crc;
+	q = buf + len;
+	for (p = buf; p < q; p++) {
+		octet = *p;
+		crc = (crc >> 8) ^ table[(crc & 0xff) ^ octet];
+	}
+	return ~crc;
+}
+
+// Returns hash value of string
+int hash(const char *s) {
+	return rc_crc32(0, s, strlen(s));
 }
 
