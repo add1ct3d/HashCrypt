41f5f1610e777a9aea01d262a993eebdf9ad5388
Isaacdelly <isaacdelly@gmail.com>
2018-01-17 20:46:22 -0800

    TODO E & D

--- hashcrypt.c
+++ hashcrypt.c
@@ -29,14 +29,21 @@ int main(int argc, char* argv[]) {
 
     // Call encrypt or decrypt depending command
     if (argc > 4) {
-        if (!isFile(argv[3])) {
+        if (!isFile(stripDash(argv[3]))) {
             printf("error locating file: %s\n", argv[3]);
             exit(1); }
 
+        char* seed = stripDash(argv[4]);
+        if (argc > 5) {
+            int i;
+            for (i=5; i < argc; i++) {
+                strcat(seed, argv[5]); }
+        }
+
         if (strcmp(argv[2], "-encrypt") == 0) {
-            encrypt(stripDash(argv[3]), stripDash(argv[4])); }
+            encrypt(stripDash(argv[3]), seed); }
         else if (strcmp(argv[2], "-decrypt") == 0) {
-            decrypt(stripDash(argv[3]), stripDash(argv[4])); }
+            decrypt(stripDash(argv[3]), seed); }
         else {
             printf("Command not recognized: %s\n\n", argv[2]);
             printf("Expected Command: gcc hashcrypt.c -encrypt -file.txt -seed\n\n");
@@ -45,9 +52,11 @@ int main(int argc, char* argv[]) {
     return 0;
 }
 
-void encrypt(char* filename, char* seed) {}
+void encrypt(char* filename, char* seed) {
+    printf("ENCRYPT:\nFile Name: %s\nSeed: %s\n\n", filename, seed); }
 
-void decrypt(char* filename, char* seed) {}
+void decrypt(char* filename, char* seed) {
+    printf("DECRYPT:\nFile Name: %s\nSeed: %s\n\n", filename, seed); }
 
 // Returns the number of bytes in a file
 long getFileSize(char* filename) {
@@ -76,3 +85,5 @@ bool isFile(char* filename) {
 char* stripDash(char* string) {
     return strchr(string, string[1]);
 }
+
+
