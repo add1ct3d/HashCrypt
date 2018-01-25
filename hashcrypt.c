10441e0cda98640e9779076c38123a9c64e1145a
Isaacdelly <isaacdelly@gmail.com>
2018-01-24 22:12:41 -0800

    Organize code

--- hashcrypt.c
+++ hashcrypt.c
@@ -1,10 +1,11 @@
+// HashCrypt Secure File Encryption
+// Made by Isaac Delly
+// https://github.com/Isaacdelly/HashCrypt
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
-#include <math.h>
-#include <conio.h>
-#include <inttypes.h>
 
 void encrypt(char*, char*);
 void decrypt(char* filename, char* seed);
@@ -14,49 +15,52 @@ char* stripDash(char* string);
 int main(int argc, char* argv[]) {
 
     // Exit program if command is incomplete
-    if (argc == 1) {
+    if (argc == 2) {
         printf("Command required\n\n");
         printf("Expected Command: gcc hashcrypt.c -encrypt -file.txt -seed\n\n");
         exit(1); }
-    if (argc == 2) {
+    if (argc == 3) {
         printf("File name required\n\n");
         printf("Expected Command: gcc hashcrypt.c -encrypt -file.txt -seed\n\n");
         exit(1); }
-    if (argc == 3) {
+    if (argc == 4) {
         printf("Seed required\n\n");
         printf("Expected Command: gcc hashcrypt.c -encrypt -file.txt -seed\n\n");
         exit(1); }
 
-    // Call encrypt or decrypt depending command
-    if (argc > 4) {
-        if (!isFile(stripDash(argv[3]))) {
-            printf("error locating file: %s\n", stripDash(argv[3]));
-            exit(1); }
+    // Check if file exists
+    if (!isFile(stripDash(argv[3]))) {
+        printf("error locating file: %s\n", stripDash(argv[3]));
+        exit(1); }
 
-        char* seed = stripDash(argv[4]);
-        if (argc > 5) {
-            int i;
-            for (i=5; i < argc; i++) {
-                strcat(seed, " ");
-                strcat(seed, stripDash(argv[i]));
-            }
+    // Combine seed args to one string
+    char* seed = stripDash(argv[4]);
+    if (argc > 5) {
+        int i;
+        for (i=5; i < argc; i++) {
+            strcat(seed, " ");
+            strcat(seed, stripDash(argv[i]));
         }
-
-        if (strcmp(stripDash(argv[2]), "encrypt") == 0) {
-            encrypt(stripDash(argv[3]), seed); }
-        else if (strcmp(stripDash(argv[2]), "decrypt") == 0) {
-            decrypt(stripDash(argv[3]), seed); }
-        else {
-            printf("Command not recognized: %s\n\n", argv[2]);
-            printf("Expected Command: gcc hashcrypt.c -encrypt -file.txt -seed\n\n");
-            exit(1); }
     }
+
+    // Call encrypt or decrypt
+    if (strcmp(stripDash(argv[2]), "encrypt") == 0) {
+        encrypt(stripDash(argv[3]), seed); }
+    else if (strcmp(stripDash(argv[2]), "decrypt") == 0) {
+        decrypt(stripDash(argv[3]), seed); }
+    else {
+        printf("Command not recognized: %s\n\n", argv[2]);
+        printf("Expected Command: gcc hashcrypt.c -encrypt -file.txt -seed\n\n");
+        exit(1); }
+
     return 0;
 }
 
+// Encrypt File
 void encrypt(char* filename, char* seed) {
-    printf("ENCRYPT:\nFile Name: %s\nSeed: %s\n\n", filename, seed); }
+    printf("ENCRYPT:\nFile Name: %s\nSeed: %s\n", filename, seed); }
 
+// Decrypt File
 void decrypt(char* filename, char* seed) {
     printf("DECRYPT:\nFile Name: %s\nSeed: %s\n", filename, seed); }
 
