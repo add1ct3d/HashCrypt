d1c75950182dc6a1ce04ee9b4c3c9fd137105557
Isaacdelly <isaacdelly@gmail.com>
2018-01-27 16:29:09 -0800

    Organized Code

--- hashcrypt.c
+++ hashcrypt.c
@@ -16,22 +16,13 @@ char* stripDash (char* string);
 int main (int argc, char* argv[]) {
 
     // Exit program if command is incomplete
-    if (argc == 2) {
-        printf("Command required\n\n");
-        printf("Expected Command: gcc hashcrypt.c -encrypt -file.txt -seed\n\n");
-        exit(1); }
-    if (argc == 3) {
-        printf("File name required\n\n");
-        printf("Expected Command: gcc hashcrypt.c -encrypt -file.txt -seed\n\n");
-        exit(1); }
-    if (argc == 4) {
-        printf("Seed required\n\n");
-        printf("Expected Command: gcc hashcrypt.c -encrypt -file.txt -seed\n\n");
+    if (argc < 5) {
+        printf("ERROR: Unable To Process Command\nExpected Command: gcc hashcrypt.c -encrypt -file.txt -seed\n");
         exit(1); }
 
     // Check if file exists
     if (!isFile(stripDash(argv[3]))) {
-        printf("error locating file: %s\n", stripDash(argv[3]));
+        printf("ERROR: Error Locating File %s\n", stripDash(argv[3]));
         exit(1); }
 
     // Combine seed args to one string
@@ -50,8 +41,7 @@ int main (int argc, char* argv[]) {
     else if (strcmp(stripDash(argv[2]), "decrypt") == 0) {
         decrypt(stripDash(argv[3]), seed); }
     else {
-        printf("Command not recognized: %s\n\n", argv[2]);
-        printf("Expected Command: gcc hashcrypt.c -encrypt -file.txt -seed\n\n");
+        printf("ERROR: Unable To Process Command\nExpected Command: gcc hashcrypt.c -encrypt -file.txt -seed\n");
         exit(1); }
 
     return 0;
